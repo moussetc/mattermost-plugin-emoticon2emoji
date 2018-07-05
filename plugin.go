11ce034fb3f31f39f27fec045b38d791c68da07e
moussetc <moussetc@users.noreply.github.com>
2018-07-05 21:29:26 +0200

    detect alone emoticon in message

--- plugin.go
+++ plugin.go
@@ -20,27 +20,44 @@ type Emoticon2EmojiPlugin struct {
 // TODO 1 : complete list with https://en.wikipedia.org/wiki/List_of_emoticons
 // TODO 2 : make it configurable with serialisation in the settings
 var matches = map[string]string{
-	":)": "slightly_smiling_face",
-	":D": "smiley",
-	";)": "wink",
-	":(": "slightly_frowning_face",
+	"XD":  "laughing",
+	"O:)": "angel",
+}
+
+// OnActivate register the plugin command
+func (p *Emoticon2EmojiPlugin) OnActivate(api plugin.API) error {
+	p.api = api
+	p.enabled = true
+
+	return nil
+}
+
+// OnDeactivate handles plugin deactivation
+func (p *Emoticon2EmojiPlugin) OnDeactivate() error {
+	p.enabled = false
+	return nil
 }
 
 // CreatePost translate emoticons in new posts
-func (p *Emoticon2EmojiPlugin)MessageWillBePosted(post *model.Post) (*model.Post, string) {
+func (p *Emoticon2EmojiPlugin) MessageWillBePosted(post *model.Post) (*model.Post, string) {
 	return p.translate(post)
 }
 
 // CreatePost translate emoticons in edited posts
-func (p *Emoticon2EmojiPlugin)MessageWillBeUpdated(newPost, oldPost *model.Post) (*model.Post, string) {
+func (p *Emoticon2EmojiPlugin) MessageWillBeUpdated(newPost, oldPost *model.Post) (*model.Post, string) {
 	return p.translate(newPost)
 }
 
 // Translate replace all the configured emoticons in a post by their equivalent as Mattermost emojis
 func (p *Emoticon2EmojiPlugin) translate(post *model.Post) (*model.Post, string) {
-	// Only consider emoticons with a space on each side
 	for emoticon, emoji := range matches {
-		post.Message = strings.Replace(post.Message, " "+emoticon+" ", " :"+emoji+": ", -1);
+		if strings.TrimSpace(post.Message) == emoticon {
+			// Only an emoticon + whitespace
+			post.Message = strings.Replace(post.Message, emoticon, ":"+emoji+":", -1)
+		} else {
+			// Only consider emoticons with a space on each side
+			post.Message = strings.Replace(post.Message, " "+emoticon+" ", " :"+emoji+": ", -1)
+		}
 	}
 	return post, ""
 }
